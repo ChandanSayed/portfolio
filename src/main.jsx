6eca189ca201b79f292077cdf12f5455ceea9e47
chandansayed <hangzhou635@gmail.com>
2023-12-12 05:43:52 +0600

    Portfolio website initializes with routes and error page

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import ErrorPage from './components/ErrorPage.jsx';
+import Root from './components/Root.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
